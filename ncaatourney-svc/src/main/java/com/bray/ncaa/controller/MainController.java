159b3bfd673d54be51ef3925d7fc04064f73af13
bobbyaray <bobby.a.ray@gmail.com>
2019-12-10 23:43:30 -0500

    Adding angular

---
+++ ncaatourney-svc/src/main/java/com/bray/ncaa/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.bray.ncaa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value={"/", "home"})
+    public String mainPage(){
+        return "forward:/index.html";
+    }
+}
